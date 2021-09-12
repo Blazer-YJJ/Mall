b68a04e8aad95cca2414c0fd22d9fcb2a6b67a07
BlazerYJJ <yangjijunA@gmail.om>
2021-09-13 00:07:53 +0800

    商品详情页吸顶功能实现

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,15 +1,38 @@
 <template>
-    <div>
-        product
+    <div class="product">
+        <product-param>
+            <template v-slot:bye>
+                <button class="btn">立即购买</button>
+            </template>
+        </product-param>
+        <div class="h">
+            <div>aa</div>
+            <div>bb</div>
+            <div>cc</div>
+            <div>dd</div>
+        </div>
     </div>
 </template>
 
 <script>
+import ProductParam from "../components/ProductParam";
 export default {
-    name: "product"
+    name: "product",
+    components: {ProductParam},
+    comments:{
+        ProductParam
+    }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+    .product{
+        .h{
+            width: 100%;
+            div{
+                width: 100%;
+                height: 200px;
+            }
+        }
+    }
 </style>
